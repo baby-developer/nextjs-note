c32ca0c8fcdfb92782fc29dc578df61a4a97d42c
baby-developer <queeh316@hanmail.net>
2023-10-26 14:11:31 +0900

    페이지 미리 생성 (generateStaticParams)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,12 @@
 type Props = {
-  params: {
-    slug: string;
-  };
+  params: { slug: string };
 };
 
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} Page</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map(product => ({ slug: product }));
 }
