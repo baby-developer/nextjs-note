bb2bd5b3b397a5196a750aab8428265a6ebd59c0
baby-developer <queeh316@hanmail.net>
2023-10-26 14:51:54 +0900

    Not Found 페이지

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation';
+
 type Props = {
   params: { slug: string };
 };
 
 export default function PantsPage({ params }: Props) {
+  if (params.slug === 'nothing') {
+    notFound();
+  }
   return <h1>{params.slug} Page</h1>;
 }
 
