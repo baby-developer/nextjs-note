4af6bb99a2e63f512cc1e6ee0d9d49666af33fe0
baby-developer <queeh316@hanmail.net>
2023-10-28 10:32:49 +0900

    Image, font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
-import styles from './page.module.css';
 import os from 'os'; // 노드 APIs (서버 컴포넌트에서 사용 가능)
 import Counter from '@/components/Counter';
-import path from 'path';
 
 export default function Home() {
   console.log('안녕? - 서버');
@@ -12,6 +10,12 @@ export default function Home() {
     <>
       <h1>홈페이지다!!</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={400}
+        height={400}
+      />
     </>
   );
 }
